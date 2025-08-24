a49ee62f4b1e15a1b66cf6bc67839d4103d32511
dh_0419 <ekgus419@gmail.com>
2025-08-24 18:37:39 +0900

    Update data.sql

--- fileblock-backend/src/main/resources/data.sql
+++ fileblock-backend/src/main/resources/data.sql
@@ -1,6 +1,8 @@
 INSERT INTO fixed_extension (extension, is_blocked, created_at, updated_at) VALUES
-('exe', false, now(), now()),
-('sh', false, now(), now()),
 ('bat', false, now(), now()),
 ('cmd', false, now(), now()),
+('com', false, now(), now()),
+('cpl', false, now(), now()),
+('exe', false, now(), now()),
+('scr', false, now(), now()),
 ('js', false, now(), now());
